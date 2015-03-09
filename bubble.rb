a18f75761d3aa8162fe108ef9b507a6ed8c650aa
stevegriffin <stephenmgriffin80@gmail.com>
2015-03-09 18:59:52 -0400

    ruby bubble sort

---
+++ bubble.rb
@@ -0,0 +1,15 @@
+#Bubble sort
+
+
+def self.sort(keys)
+  0.upto(keys.size-1) do |i|
+    (keys.size-1).downto(i+1) do |j|
+      (keys[j], keys[j-1] = keys[j-1], keys[j]) if keys[j] < keys[j-1]
+    end
+  end
+  keys
+end
+
+collection = [100,15,61,17,38,12]
+
+puts sort(collection)
