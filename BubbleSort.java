89962a1de1a9b65c19b181917f674506c1f3cc3f
stevegriffin <stephenmgriffin80@gmail.com>
2015-03-11 20:43:36 -0400

    added start of java bubble sort

---
+++ BubbleSort.java
@@ -0,0 +1,24 @@
+public class BubbleSort {
+	public static void main(String[] args) {
+
+		int[] values = {2,100,32,18,17,5};
+		Sort b = new Sort();
+		b.sort(values);
+	}
+}
+
+class Sort {
+
+	public void sort(int[] numbers) {
+
+		for (int i = numbers.length - 1; i > 1; i-- {
+
+			for (int x = 0; x < i; x++) {
+				//TBD
+			}
+
+			//System.out.println(numbers[i]);
+		}
+	}
+}
+
