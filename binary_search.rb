a0315cfe766c560842e2eab7458331277554c1b3
stevegriffin <stephenmgriffin80@gmail.com>
2015-03-17 19:18:13 -0400

    ruby binary search functional

--- binary_search.rb
+++ binary_search.rb
@@ -3,17 +3,37 @@
 values = [1,7,14,16,55,77, 79,100,150]
 
 
-def binary_search(collection, value, high, low)
+def binary_search(collection, search_value, collection_size)
 
-  mid = collection[collection.length / 2]
+  low = 0
+  high = collection_size - 1
 
-  if value == mid
-    return collection[mid]
-  elsif value < mid
-  elsif value > mid
-  else
+  #iterate until the entire collection has been searched
+  while low <= high
+  	#set the mid value
+    mid = (low + high) / 2
+
+    # if the mid value matches the search value, return the result
+    if search_value == collection[mid]
+      return collection[mid]
+
+    # value is less than the mid, so the upper bound
+    # is set to mid - 1
+    elsif search_value < collection[mid]
+      high = mid - 1
+
+    else
+      # value is greater than the mid value,
+      # so the lower bound is set to mid + 1
+      low = mid + 1
+    end
+
+    #value was not found
+    if low > high
+      return "value not found"
+    end
+
   end
-
 end
 
-puts binary_search(values, 14, collection.length - 1, )
+puts binary_search(values, 16, values.length )
