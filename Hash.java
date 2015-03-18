c1a8e5edec110d91793d2052672c8798c0748a99
stevegriffin <stephenmgriffin80@gmail.com>
2015-03-18 18:44:51 -0400

    initial hash table java class

---
+++ Hash.java
@@ -0,0 +1,30 @@
+// Java hash table implementation
+// O(1) Average complexity for search, insertion and delete
+
+public class Hash {
+
+
+	//main method
+	public static void main(String[] args) {
+		//TBD
+	}
+
+	//method to return unique key
+	public int hashKey(String s) {
+		int value = 0;
+
+		//TBD
+		return value;
+	}
+
+	//method to insert value into hash table
+	public boolean insert(Object value) {
+		boolean result = false;
+		//TBD
+
+		return result;
+	}
+
+	
+
+}
