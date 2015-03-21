a5bf29464fca5a54d2fa7b4e5682a39aa2832cc9
stevegriffin <stephenmgriffin80@gmail.com>
2015-03-21 15:33:55 -0400

    ruby hash table implementation

--- hash.rb
+++ hash.rb
@@ -1,3 +1,49 @@
-# Hash table implementation
+require 'pry'
 
-# TBD
+# Hash table implementation
+class HashTable1
+
+  # Constructor
+  def initialize
+
+  end
+
+  # Hashing function
+  def hash_function(key)
+  	# total
+  	total = 0
+
+  	# iterate over each character
+  	key.each_char do |char|
+  		# get the ascii value
+  		ascii = char.ord
+  		# add value of character to total
+  		total += ascii
+  		# modulo 1000 (table range)
+  		total = total % 1000
+
+  	end
+
+  end
+
+  def insert
+  end
+
+  def delete
+  end
+
+  def search
+  end
+
+  def test
+  end
+
+
+end
+
+
+h = HashTable1.new
+
+# binding.pry
+
+h.hash_function("test")
